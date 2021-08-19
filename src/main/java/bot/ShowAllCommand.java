a699168d4e035ac4943ae194dbe9bc5940eefce4
Vorobeva <passant.dlm@gmail.com>
2021-08-19 13:03:14 +0200

    system.properties are added

--- src/main/java/bot/ShowAllCommand.java
+++ src/main/java/bot/ShowAllCommand.java
@@ -1,11 +1,13 @@
 package bot;
 
 import DTO.ItemDTO;
+import lombok.SneakyThrows;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import parsing.BeerParser;
 
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -19,6 +21,7 @@ public class ShowAllCommand extends ServiceCommand {
     }
 
 
+    @SneakyThrows
     @Override
     public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
         //формируем имя пользователя - поскольку userName может быть не заполнено, для этого случая используем имя и фамилию пользователя
@@ -28,15 +31,19 @@ public class ShowAllCommand extends ServiceCommand {
         HashMap<String, ItemDTO> beers = BeerParser.getActualBeers();
         StringBuilder beerList = new StringBuilder();
         for (Map.Entry<String, ItemDTO> beer : beers.entrySet()) {
+            if (!beer.getValue().getInfo().isAvailable())
+                beerList.append("~");
             beerList.append("id: ").append(beer.getValue().getId()).append(" ")
                     .append(beer.getValue().getInfo())
                     .append("\n").append(beer.getValue().getSmallVolume())
                     .append(" ").append(beer.getValue().getLargeVolume());
             if (!beer.getValue().getInfo().isAvailable())
-                beerList.append(" BEER IS NOT AVAILABLE NOW");
+                beerList.append("~");
+               // beerList.append(" BEER IS NOT AVAILABLE NOW");
                     beerList.append("\n\n");
         }
+        String answer = URLEncoder.encode(beerList.toString(), "UTF-8");
         sendAnswer(absSender, chat.getId(), this.getCommandIdentifier(), userName,
-                "Actual menu: \n" + beerList);
+                "Actual menu: \n" + answer);
     }
 }
